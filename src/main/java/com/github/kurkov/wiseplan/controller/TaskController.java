04e6457a428df5f902933f0d037dc67d7685ac81
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2017-02-24 00:41:50 +0200

    change task controller

--- src/main/java/com/github/kurkov/wiseplan/controller/TaskController.java
+++ src/main/java/com/github/kurkov/wiseplan/controller/TaskController.java
@@ -3,52 +3,65 @@ package com.github.kurkov.wiseplan.controller;
 import com.github.kurkov.wiseplan.entity.Task;
 import com.github.kurkov.wiseplan.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.Map;
-
 /**
  * @author Aleksey Kurkov. Created on 19.02.17
  * @version 1.0
  */
+@Controller
 public class TaskController {
 
-    @Autowired
     private TaskService taskService;
 
-    @RequestMapping("/index")
-    public String listContacts(Map<String, Object> map) {
+    @Autowired(required = true)
+    @Qualifier(value = "taskService")
+    public void setTaskService(TaskService taskService) {
+        this.taskService = taskService;
+    }
 
-        map.put("task", new Task());
-        map.put("taskList", taskService.taskList());
-
+    @RequestMapping(value = "/tasks", method = RequestMethod.GET)
+    public String listContacts(Model model) {
+        model.addAttribute("task", new Task());
+        model.addAttribute("taskList", taskService.taskList());
         return "task";
     }
 
     @RequestMapping("/")
     public String home() {
-        return "redirect:/index";
+        return "redirect:/tasks";
     }
 
-    @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public String addContact(@ModelAttribute("task") Task task,
-                             BindingResult result) {
+    //For add and update task both
+    @RequestMapping(value = "/task/add", method = RequestMethod.POST)
+    public String addContact(@ModelAttribute("task") Task task) {
+        if (task.getId() == 0) {
+            //new task, add it
+            taskService.addTask(task);
+        } else {
+            //existing task, call update
+            taskService.updateTask(task);
+        }
 
-        taskService.addTask(task);
-
-        return "redirect:/index";
+        return "redirect:/tasks";
     }
 
     @RequestMapping("/delete/{taskId}")
-    public String deleteContact(@PathVariable("taskId") Long taskId) {
+    public String deleteTask(@PathVariable("taskId") Long taskId) {
+        taskService.removeTask(taskId);
+        return "redirect:/tasks";
+    }
 
-        Task task = taskService.getTaskById(taskId);
-        taskService.removeTask(task);
-
-        return "redirect:/index";
+    @RequestMapping("/edit/{taskId}")
+    public String editTask(@PathVariable("taskId") Long taskId, Model model) {
+        model.addAttribute("task", taskService.getTaskById(taskId));
+        model.addAttribute("listTasks", taskService.taskList());
+        return "task";
     }
 }
